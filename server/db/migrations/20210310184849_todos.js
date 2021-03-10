1069212e0a495638181bbf9ac67f8549edc8e7aa
jordan-tuinman <jojotuijo@gmail.com>
2021-03-10 19:03:34 +1300

    fix typo in todos table

--- server/db/migrations/20210310184849_todos.js
+++ server/db/migrations/20210310184849_todos.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('todos', (table) => {
     table.increments('id')
     table.string('todo')
-    table.state('state')
+    table.string('state')
     table.boolean('complete')
   })
 }
--- server/db/seeds/todos.js
+++ server/db/seeds/todos.js
@@ -1,13 +1,12 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('table_name').del()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+        { id: 1, todo: 'Create DB', state: 'complete', complete: true },
+        { id: 2, todo: 'Complete app', state: 'active', complete: false },
+        { id: 3, todo: 'Style app', state: 'active', complete: false }
+      ])
+    })
+}
