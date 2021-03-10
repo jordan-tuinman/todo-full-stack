66ba1e39342d12bed8b0b035505fdd11c0777058
jordan-tuinman <jojotuijo@gmail.com>
2021-03-10 19:04:40 +1300

    add seed data

--- server/db/seeds/todos.js
+++ server/db/seeds/todos.js
@@ -1,9 +1,9 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('todos').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('todos').insert([
         { id: 1, todo: 'Create DB', state: 'complete', complete: true },
         { id: 2, todo: 'Complete app', state: 'active', complete: false },
         { id: 3, todo: 'Style app', state: 'active', complete: false }
