525ab588e86f25a729a6a5a0caf50fa7f2a19abd
jordan-tuinman <jojotuijo@gmail.com>
2021-03-10 19:10:15 +1300

    add basic files for serverside

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ const path = require('path')
 
 const server = express()
 
+const todos = require('./routes/todos')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+
+server.use('/', todos)
 
 module.exports = server
