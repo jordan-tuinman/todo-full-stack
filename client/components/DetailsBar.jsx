5928abd264bd8e5c55b1b659f4461cc3d2159e75
jordan-tuinman <jojotuijo@gmail.com>
2021-03-12 17:29:48 +1300

    add details bar component

--- client/components/DetailsBar.jsx
+++ client/components/DetailsBar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+// TODO: hide component when 0 todos in list
+
 function DetailsBar () {
   return (
     <>
+      <footer className="footer">
+        <span className="todo-count"><strong>0</strong> item left</span>
+        <ul className="filters">
+          <li>
+            <a className="selected" href="/">All</a>
+          </li>
+          <li>
+            <a href="/active">Active</a>
+          </li>
+          <li>
+            <a href="/completed">Completed</a>
+          </li>
+        </ul>
+        {/* TODO: hide when no completed items */}
+        <button className="clear-completed">Clear completed</button>
+      </footer>
     </>
   )
 }
--- client/index.js
+++ client/index.js
@@ -10,6 +10,8 @@ import App from './components/App'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
+// TODO: Add browser router
+
 document.addEventListener('DOMContentLoaded', () => {
   render(
     <Provider store={store}>
