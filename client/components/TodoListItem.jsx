3507a4b00272685575119a8b1f96515db37625b6
jordan-tuinman <jojotuijo@gmail.com>
2021-03-17 16:24:15 +1300

    partial work on edit feature

--- client/components/TodoListItem.jsx
+++ client/components/TodoListItem.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 
 import { removeTodo, updateTodo } from '../actions'
 
 function TodoListItem ({ dispatch, todo }) {
-  function handleToggle (evt) {
+  const [todoEdit, setEdit] = useState({
+    id: todo.id,
+    todo: todo.todo,
+    complete: todo.complete,
+    edit: false
+  })
+
+  function handleCheck (evt) {
     dispatch(updateTodo(todo.id, { complete: String(evt.target.checked) }))
+  }
+
+  function toggleEdit () {
+    setEdit({ ...todoEdit, edit: !todoEdit.edit })
+  }
+
+  function handleChange (evt) {
+    setEdit({ ...todoEdit, todo: evt.target.value })
+  }
+
+  function handleEdit () {
+
   }
 
   function handleDelete () {
@@ -16,11 +35,13 @@ function TodoListItem ({ dispatch, todo }) {
   return (
     <li key={todo.id} className={todoState}>
       <div className="view">
-        <input className="toggle" type="checkbox" onClick={handleToggle} defaultChecked={JSON.parse(todo.complete)}/>
-        <label>{todo.todo}</label>
+        <input className="toggle" type="checkbox" onClick={handleCheck} defaultChecked={JSON.parse(todo.complete)}/>
+        {!todoEdit.edit && <label onClick={toggleEdit}>{todo.todo}</label>}
         <button className="destroy" onClick={handleDelete}></button>
       </div>
-      {/* <input className="edit" value="Create a TodoMVC template" /> */}
+      <form onSubmit={handleEdit}>
+        {todoEdit.edit && <input onChange={handleChange} className="edit" value={todoEdit.todo}/> }
+      </form>
     </li>
   )
 }
